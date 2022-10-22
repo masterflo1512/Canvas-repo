396a194534fb7c7e837b3796569fba2436c2bb41
masterflo1512 <sakharovbig@gmail.com>
2022-10-22 20:58:54 +0300

    add

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
@@ -7,61 +8,44 @@ let currentLineStartPointPosition = [];
 let lines = [];
 
 
-class Point {
-    constructor(x,y) {
-        this.x = x;
-        this.y = y;
-    }
-    getCursorPosition(e) {
-        const rect = canvas.getBoundingClientRect()
-        x = e.clientX - rect.left;
-        y = e.clientY - rect.top;
-        return [x, y];
-    }
+canvas.addEventListener('click', drawLine);
+
+function getCursorPosition(e) {
+    const rect = canvas.getBoundingClientRect()
+    x = e.clientX - rect.left;
+    y = e.clientY - rect.top;
+    return [x, y];
 }
 
+function drawLine(e) {
+    let [x, y] = getCursorPosition(e);
+    let pointsIntersection = {};
+    if (!isFirstPointSelected) {
+        isFirstPointSelected = true;
+        currentLineStartPointPosition = [x, y];
 
-class Line {
-    constructor(x,y) {
-        this.x = x;
-        this.y = y;
-    }
+    } else {
+        context.beginPath();
+        context.moveTo(currentLineStartPointPosition[0], currentLineStartPointPosition[1]);
+        context.lineTo(x, y);
+        context.lineWidth = 1;
+        context.stroke();
+        isFirstPointSelected = false;
 
-    
-    drawLine() {
-        
-        let [x, y] = getCursorPosition(e);
-        let pointsIntersection = {};
-        canvas.addEventListener('click', e =>{
-            if (!isFirstPointSelected) {
-                isFirstPointSelected = true;
-                currentLineStartPointPosition = [x, y];
-    
-            } else {
-            context.beginPath();
-            context.moveTo(currentLineStartPointPosition[0], currentLineStartPointPosition[1]);
-            context.lineTo(x, y);
+        for (let i = 0; i < lines.length; i++) {
+            console.log(lines);
+            pointsIntersection = lineIntersect(currentLineStartPointPosition[0], currentLineStartPointPosition[1], x, y, lines[i].x1, lines[i].y1, lines[i].x2, lines[i].y2);
+            drawCircle(pointsIntersection);
+            context.strokeStyle = "black";
             context.lineWidth = 1;
-            context.stroke();
-            isFirstPointSelected = false;
-            
-            for (let i = 0; i < lines.length; i++) {
-                console.log(lines);
-                pointsIntersection = lineIntersect(currentLineStartPointPosition[0], currentLineStartPointPosition[1], x, y, lines[i].x1, lines[i].y1, lines[i].x2, lines[i].y2); 
-                drawCircle(pointsIntersection);
-                context.strokeStyle = "black";
-                context.lineWidth = 1;
-            }  
-            lines.push({ x1: currentLineStartPointPosition[0], y1: currentLineStartPointPosition[1], x2: x, y2: y });     
         }
-    })
-    };
-}
+        lines.push({ x1: currentLineStartPointPosition[0], y1: currentLineStartPointPosition[1], x2: x, y2: y });
+    }
+};
 
-
-function drawCircle({x,y}) {
+function drawCircle({ x, y }) {
     context.beginPath();
-    context.arc(x, y, 5, 0, 2 * Math.PI,false);
+    context.arc(x, y, 5, 0, 2 * Math.PI, false);
     context.lineWidth = 5;
     context.strokeStyle = "red";
     context.stroke();
@@ -76,41 +60,42 @@ window.oncontextmenu = function () {
 
 var eps = 0.0000001;
 function between(a, b, c) {
-    return a-eps <= b && b <= c+eps;
+    return a - eps <= b && b <= c + eps;
 }
 
-function lineIntersect(x1, y1, x2, y2, x3, y3, x4, y4) {   
-    var x=((x1*y2-y1*x2)*(x3-x4)-(x1-x2)*(x3*y4-y3*x4)) /
-            ((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4));
-    var y=((x1*y2-y1*x2)*(y3-y4)-(y1-y2)*(x3*y4-y3*x4)) /
-            ((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4));
-    if (isNaN(x)||isNaN(y)) {
+function lineIntersect(x1, y1, x2, y2, x3, y3, x4, y4) {
+    var x = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) /
+        ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
+    var y = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) /
+        ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
+    if (isNaN(x) || isNaN(y)) {
         return false;
     } else {
-        if (x1>=x2) {
-            if (!between(x2, x, x1)) {return false;}
+        if (x1 >= x2) {
+            if (!between(x2, x, x1)) { return false; }
         } else {
-            if (!between(x1, x, x2)) {return false;}
+            if (!between(x1, x, x2)) { return false; }
         }
-        if (y1>=y2) {
-            if (!between(y2, y, y1)) {return false;}
+        if (y1 >= y2) {
+            if (!between(y2, y, y1)) { return false; }
         } else {
-            if (!between(y1, y, y2)) {return false;}
+            if (!between(y1, y, y2)) { return false; }
         }
-        if (x3>=x4) {
-            if (!between(x4, x, x3)) {return false;}
+        if (x3 >= x4) {
+            if (!between(x4, x, x3)) { return false; }
         } else {
-            if (!between(x3, x, x4)) {return false;}
+            if (!between(x3, x, x4)) { return false; }
         }
-        if (y3>=y4) {
-            if (!between(y4, y, y3)) {return false;}
+        if (y3 >= y4) {
+            if (!between(y4, y, y3)) { return false; }
         } else {
-            if (!between(y3, y, y4)) {return false;}
+            if (!between(y3, y, y4)) { return false; }
         }
     }
-    return {x: x, y: y};
+    return { x: x, y: y };
 }
 
-document.getElementById('collapse').addEventListener('click', () =>{
+document.getElementById('collapse').addEventListener('click', () => {
     context.clearRect(0, 0, canvas.width, canvas.height);
 })
+
